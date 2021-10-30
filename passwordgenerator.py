33cdb0ab2e78a60f53994a4161c602249c208664
dreamcoder88 <70472973+dreamcoder88@users.noreply.github.com>
2021-10-30 14:39:22 +0530

    Update passwordgenerator.py

--- passwordgenerator.py
+++ passwordgenerator.py
@@ -1,59 +1,102 @@
+# Python program to generate random
+# password using Tkinter module
 import random
-import array
+import pyperclip
+from tkinter import *
+from tkinter.ttk import *
 
-# maximum length of password needed
-# this can be changed to suit your password length
-MAX_LEN = 12
+# Function for calculation of password
+def low():
+	entry.delete(0, END)
 
-# declare arrays of the character that we need in out password
-# Represented as chars to enable easy string concatenation
-DIGITS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-LOCASE_CHARACTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-					'i', 'j', 'k', 'm', 'n', 'o', 'p', 'q',
-					'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
-					'z']
+	# Get the length of password
+	length = var1.get()
 
-UPCASE_CHARACTERS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
-					'I', 'J', 'K', 'M', 'N', 'O', 'p', 'Q',
-					'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
-					'Z']
+	lower = "abcdefghijklmnopqrstuvwxyz"
+	upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+	digits = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 !@#$%^&*()"
+	password = ""
 
-SYMBOLS = ['@', '#', '$', '%', '=', ':', '?', '.', '/', '|', '~', '>',
-		'*', '(', ')', '<']
+	# if strength selected is low
+	if var.get() == 1:
+		for i in range(0, length):
+			password = password + random.choice(lower)
+		return password
 
-# combines all the character arrays above to form one array
-COMBINED_LIST = DIGITS + UPCASE_CHARACTERS + LOCASE_CHARACTERS + SYMBOLS
+	# if strength selected is medium
+	elif var.get() == 0:
+		for i in range(0, length):
+			password = password + random.choice(upper)
+		return password
 
-# randomly select at least one character from each character set above
-rand_digit = random.choice(DIGITS)
-rand_upper = random.choice(UPCASE_CHARACTERS)
-rand_lower = random.choice(LOCASE_CHARACTERS)
-rand_symbol = random.choice(SYMBOLS)
-
-# combine the character randomly selected above
-# at this stage, the password contains only 4 characters but
-# we want a 12-character password
-temp_pass = rand_digit + rand_upper + rand_lower + rand_symbol
+	# if strength selected is strong
+	elif var.get() == 3:
+		for i in range(0, length):
+			password = password + random.choice(digits)
+		return password
+	else:
+		print("Please choose an option")
 
 
-# now that we are sure we have at least one character from each
-# set of characters, we fill the rest of
-# the password length by selecting randomly from the combined
-# list of character above.
-for x in range(MAX_LEN - 4):
-	temp_pass = temp_pass + random.choice(COMBINED_LIST)
+# Function for generation of password
+def generate():
+	password1 = low()
+	entry.insert(10, password1)
 
-	# convert temporary password into array and shuffle to
-	# prevent it from having a consistent pattern
-	# where the beginning of the password is predictable
-	temp_pass_list = array.array('u', temp_pass)
-	random.shuffle(temp_pass_list)
 
-# traverse the temporary password array and append the chars
-# to form the password
-password = ""
-for x in temp_pass_list:
-		password = password + x
-		
-# print out password
-print(password)
+# Function for copying password to clipboard
+def copy1():
+	random_password = entry.get()
+	pyperclip.copy(random_password)
+
+
+# Main Function
+
+# create GUI window
+root = Tk()
+var = IntVar()
+var1 = IntVar()
+
+# Title of your GUI window
+root.title("Random Password Generator")
+
+# create label and entry to show
+# password generated
+Random_password = Label(root, text="Password")
+Random_password.grid(row=0)
+entry = Entry(root)
+entry.grid(row=0, column=1)
+
+# create label for length of password
+c_label = Label(root, text="Length")
+c_label.grid(row=1)
+
+# create Buttons Copy which will copy
+# password to clipboard and Generate
+# which will generate the password
+copy_button = Button(root, text="Copy", command=copy1)
+copy_button.grid(row=0, column=2)
+generate_button = Button(root, text="Generate", command=generate)
+generate_button.grid(row=0, column=3)
+
+# Radio Buttons for deciding the
+# strength of password
+# Default strength is Medium
+radio_low = Radiobutton(root, text="Low", variable=var, value=1)
+radio_low.grid(row=1, column=2, sticky='E')
+radio_middle = Radiobutton(root, text="Medium", variable=var, value=0)
+radio_middle.grid(row=1, column=3, sticky='E')
+radio_strong = Radiobutton(root, text="Strong", variable=var, value=3)
+radio_strong.grid(row=1, column=4, sticky='E')
+combo = Combobox(root, textvariable=var1)
+
+# Combo Box for length of your password
+combo['values'] = (8, 9, 10, 11, 12, 13, 14, 15, 16,
+				17, 18, 19, 20, 21, 22, 23, 24, 25,
+				26, 27, 28, 29, 30, 31, 32, "Length")
+combo.current(0)
+combo.bind('<<ComboboxSelected>>')
+combo.grid(column=1, row=1)
+
+# start the GUI
+root.mainloop()
